12ec9917344f169abb1491e8b66c89171ff035c7
shadow <krushna.gutte78@gmail.com>
2024-11-03 10:05:08 +0530

    I-design fix for the homepage top level comp aka HomePage

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -3,13 +3,13 @@ import RightSection from "./RightSection";
 export default function HomePage() {
   return (
     <div className="">
-      <div className="relative bg-gradient-to-r from-[#21214e] to-[#0F1031] w-screen h-[1300px]  xl:h-[1000px]"></div>
+      <div className="relative bg-gradient-to-r from-[#21214e] to-[#0F1031] w-screen h-[1300px]  lg:h-[1000px]"></div>
       <div className="w-screen flex justify-center items-center">
-        <div className="flex flex-col lg:flex-row absolute top-0  lg:mt-32   xl:w-[1700px] gap-10 lg:gap-0 ">
-          <div className="w-full h:1/2 xl:w-1/2 xl:h-full p-1 mt-16">
+        <div className="flex flex-col lg:flex-row absolute top-0  lg:mt-32   lg:w-[1700px] gap-10 lg:gap-0 ">
+          <div className="w-full h:1/2 lg:w-1/2 lg:h-full p-1 mt-16">
             <LeftSection />
           </div>
-          <div className="w-full h:1/2 xl:w-1/2 xl:h-full">
+          <div className="w-full h:1/2 lg:w-1/2 lg:h-full">
             <RightSection />
           </div>
         </div>
